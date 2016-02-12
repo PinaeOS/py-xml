05cb04833d8c7bb950d20cbacaede9d1fd50e76d
Yugeng Hui <interhuiyg@163.com>
2016-02-12 12:15:24 +0800

    Update xml_parser test case

--- py_xml_test/xml_parser_test.py
+++ py_xml_test/xml_parser_test.py
@@ -8,4 +8,20 @@ class XmlParserTest(unittest.TestCase):
     def test_parse(self):
         parser = xml_parser.XmlParser()
         result = parser.parse('test.xml')
-        print result
+
+        self.assertEqual(len(result.get('root').get('group').get('family')), 2)
+        self.assertEqual(result.get('root').get('group').get('family')[0].get('_node_'), 'green')
+        self.assertEqual(result.get('root').get('group').get('family')[1].get('_node_'), 'cooker')
+
+        self.assertEqual(len(result.get('root').get('parent')[0].get('child')), 3)
+        self.assertEqual(len(result.get('root').get('parent')[1].get('child')), 2)
+        
+        self.assertEqual(result.get('root').get('parent')[0].get('child')[0].get('name').get('_attr_'), 'jim')
+        self.assertEqual(result.get('root').get('parent')[0].get('child')[0].get('age').get('_attr_'), '23')
+        self.assertEqual(result.get('root').get('parent')[0].get('child')[0].get('address').get('_node_'), 'Shenzhen')
+        self.assertEqual(result.get('root').get('parent')[0].get('child')[0].get('phone').get('_node_'), '18607578001')
+        
+        self.assertEqual(result.get('root').get('parent')[1].get('child')[1].get('name').get('_attr_'), 'tom')
+        self.assertEqual(result.get('root').get('parent')[1].get('child')[1].get('age').get('_attr_'), '45')
+        self.assertEqual(result.get('root').get('parent')[1].get('child')[1].get('address').get('_node_'), 'Shanghai')
+        self.assertEqual(result.get('root').get('parent')[1].get('child')[1].get('phone').get('_node_'), '13391562334')
